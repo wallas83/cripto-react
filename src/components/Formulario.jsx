f055556bd0becf1a9194cf5c8a9777ffaeeab19b
wallas <whuanca06@gmail.com>
2023-07-08 15:58:26 -0400

    adding data from criptocompare

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelectMonedas } from '../hooks/useSelectMonedas'
+import { monedas } from '../data/monedas'
+
 
 const InputSubmit = styled.input`
 background-color: #9497FF;
@@ -12,6 +15,7 @@ text-transform: uppercase;
 font-size: 20px;
 border-radius: 5px;
 transition: background-color .3s ease;
+margin-top: 30px;
 
 &:hover{
     background-color: #7A7DFE;
@@ -19,12 +23,28 @@ transition: background-color .3s ease;
 }
 `
 export const Formulario = () => {
-  return (
-    <form>
-        <InputSubmit
-        type='submit'
-        value='Cotizar'
-        />
-    </form>
-  )
+
+    const [moneda, SelectMonedas] = useSelectMonedas('Elige tu moneda', monedas);
+
+    useEffect(() => {
+           const consultarApi = async () => {
+            const url = "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD";
+            const respuesta = await fetch(url);
+            const resutlado = await respuesta.json();
+            console.log(resutlado.Data);
+        }
+
+        consultarApi();
+    }, [])
+
+    return (
+        <form>
+            <SelectMonedas />
+
+            <InputSubmit
+                type='submit'
+                value='Cotizar'
+            />
+        </form>
+    )
 }
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled"
+import { useState } from "react"
+
+
+const Label = styled.label`
+    color: #FFF;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select = styled.select`
+width: 100%;
+font-size: 18px;
+padding: 14px;
+border-radius: 10px;
+
+` 
+
+export const useSelectMonedas = (label, opciones) => {
+    const [state, setState] =  useState('');
+
+
+    // luego del arrow function se usa parentesisi para llamar ujn componentes o para que se renderice
+    const SelectMonedas = () => (
+        <>
+            <Label>{label}</Label>
+            <Select
+                value={state}
+                onChange={ e => setState(e.target.value)}
+            >
+                <option value=''>Selecione</option>
+            
+            {
+                opciones.map( opcion => (
+                    <option
+                        key= {opcion.id}
+                        value = {opcion.id}
+                    >
+                        {opcion.nombre}
+                    </option>
+                ))
+            }
+            </Select>
+        </>
+
+    )
+
+    return [state, SelectMonedas]
+
+}
